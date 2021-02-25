ff3918095dc4570b3f0ebb28fd1500ed9c962cfd
ShinNagamine <78636399+ShinNagamine@users.noreply.github.com>
2021-02-25 16:10:54 +0900

    Add files via upload

--- script2.js
+++ script2.js
@@ -4,120 +4,122 @@ const Peer = window.Peer;
 const SKYWAY_KEY = "4bc300c2-d192-4bfa-aa15-45bfb80d6c1d";
 
 (async function main() {
-  const localVideo = document.getElementById('js-local-stream');
-  const joinTrigger = document.getElementById('js-join-trigger');
-  const leaveTrigger = document.getElementById('js-leave-trigger');
-  const remoteVideos = document.getElementById('js-remote-streams');
-  const roomId = document.getElementById('js-room-id');
-  const roomMode = document.getElementById('js-room-mode');
-  const localText = document.getElementById('js-local-text');
-  const sendTrigger = document.getElementById('js-send-trigger');
-  const messages = document.getElementById('js-messages');
-  const meta = document.getElementById('js-meta');
-  const sdkSrc = document.querySelector('script[src*=skyway]');
+	// VIDEO要素
+	const localVideo = document.getElementById('localVideo');
+	const remoteVideos = document.getElementById('remoteVideos');
 
-  meta.innerText = `
-    UA: ${navigator.userAgent}
-    SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
-  `.trim();
+	// ルームID、[参加]、[退出]ボタン
+	const roomId = document.getElementById('roomId');
+	const joinBtn = document.getElementById('joinBtn');
+	const leaveBtn = document.getElementById('leaveBtn');
 
-  const getRoomModeByHash = () => (location.hash === '#sfu' ? 'sfu' : 'mesh');
+	// メッセージ入力欄、[送信]ボタン、メッセージ一覧
+	const messageText = document.getElementById('messageText');
+	const sendBtn = document.getElementById('sendBtn');
+	const messages = document.getElementById('messageArea');
 
-  roomMode.textContent = getRoomModeByHash();
-  window.addEventListener(
-    'hashchange',
-    () => (roomMode.textContent = getRoomModeByHash())
-  );
+	// カメラON
+	const localStream = await navigator.mediaDevices
+		.getUserMedia({
+			audio: true,
+			video: true,
+		})
+		.catch(console.error);
 
-  const localStream = await navigator.mediaDevices
-    .getUserMedia({
-      audio: true,
-      video: true,
-    })
-    .catch(console.error);
+	// Render local stream
+	localVideo.muted = true;
+	localVideo.srcObject = localStream;
+	localVideo.playsInline = true;
+	await localVideo.play().catch(console.error);
 
-  // Render local stream
-  localVideo.muted = true;
-  localVideo.srcObject = localStream;
-  localVideo.playsInline = true;
-  await localVideo.play().catch(console.error);
+	// ピア接続
+	const peer = (window.peer = new Peer({
+		key: SKYWAY_KEY,
+		debug: 3,
+	}));
 
-  // eslint-disable-next-line require-atomic-updates
-  const peer = (window.peer = new Peer({
-    key: SKYWAY_KEY,
-    debug: 3,
-  }));
 
-  // Register join handler
-  joinTrigger.addEventListener('click', () => {
-    // Note that you need to ensure the peer has connected to signaling server
-    // before using methods of peer instance.
-    if (!peer.open) {
-      return;
-    }
+	// [参加]ボタンクリックイベントリスナー追加
+	joinBtn.addEventListener('click', () => {
+		// Note that you need to ensure the peer has connected to signaling server
+		// before using methods of peer instance.
+		if (!peer.open) {
+			return;
+		}
 
-    const room = peer.joinRoom(roomId.value, {
-      mode: getRoomModeByHash(),
-      stream: localStream,
-    });
+		const room = peer.joinRoom(roomId.value, {
+			mode: 'sfu',
+			stream: localStream,
+		});
 
-    room.once('open', () => {
-      messages.textContent += '=== You joined ===\n';
-    });
-    room.on('peerJoin', peerId => {
-      messages.textContent += `=== ${peerId} joined ===\n`;
-    });
+		// 自身参加時
+		room.once('open', () => {
+			messages.textContent += '=== あなたが参加しました。 ===\n';
+		});
 
-    // Render remote stream for new peer join in the room
-    room.on('stream', async stream => {
-      const newVideo = document.createElement('video');
-      newVideo.srcObject = stream;
-      newVideo.playsInline = true;
-      // mark peerId to find it later at peerLeave event
-      newVideo.setAttribute('data-peer-id', stream.peerId);
-      remoteVideos.append(newVideo);
-      await newVideo.play().catch(console.error);
-    });
+		// 他メンバー参加時
+		room.on('peerJoin', peerId => {
+			messages.textContent += `=== ${peerId} が参加しました。 ===\n`;
+		});
 
-    room.on('data', ({ data, src }) => {
-      // Show a message sent to the room and who sent
-      messages.textContent += `${src}: ${data}\n`;
-    });
+		// Render remote stream for new peer join in the room
+		// 他メンバー映像受信時
+		room.on('stream', async stream => {
+			// 要素新規作成
+			const newVideo = document.createElement('video');
+			newVideo.srcObject = stream;
+			newVideo.playsInline = true;
 
-    // for closing room members
-    room.on('peerLeave', peerId => {
-      const remoteVideo = remoteVideos.querySelector(
-        `[data-peer-id="${peerId}"]`
-      );
-      remoteVideo.srcObject.getTracks().forEach(track => track.stop());
-      remoteVideo.srcObject = null;
-      remoteVideo.remove();
+			// mark peerId to find it later at peerLeave event
+			newVideo.setAttribute('data-peer-id', stream.peerId);
+			remoteVideos.append(newVideo);
+			await newVideo.play().catch(console.error);
+		});
 
-      messages.textContent += `=== ${peerId} left ===\n`;
-    });
+		// メッセージ受信時
+		room.on('data', ({ data, src }) => {
+			// Show a message sent to the room and who sent
+			messages.textContent += `${src}: ${data}\n`;
+		});
 
-    // for closing myself
-    room.once('close', () => {
-      sendTrigger.removeEventListener('click', onClickSend);
-      messages.textContent += '== You left ===\n';
-      Array.from(remoteVideos.children).forEach(remoteVideo => {
-        remoteVideo.srcObject.getTracks().forEach(track => track.stop());
-        remoteVideo.srcObject = null;
-        remoteVideo.remove();
-      });
-    });
+		// for closing room members
+		// メンバー退出時
+		room.on('peerLeave', peerId => {
+			const remoteVideo = remoteVideos.querySelector(
+				`[data-peer-id="${peerId}"]`
+			);
+			remoteVideo.srcObject.getTracks().forEach(track => track.stop());
+			remoteVideo.srcObject = null;
+			remoteVideo.remove();
 
-    sendTrigger.addEventListener('click', onClickSend);
-    leaveTrigger.addEventListener('click', () => room.close(), { once: true });
+			messages.textContent += `=== ${peerId} が退出しました。 ===\n`;
+		});
 
-    function onClickSend() {
-      // Send message to all of the peers in the room via websocket
-      room.send(localText.value);
+		// for closing myself
+		// クローズ時
+		room.once('close', () => {
+			sendBtn.removeEventListener('click', onClickSend);
+			messages.textContent += '=== あなたが退出しました。 ===\n';
+			Array.from(remoteVideos.children).forEach(remoteVideo => {
+				// カメラを停止し、要素削除
+				remoteVideo.srcObject.getTracks().forEach(track => track.stop());
+				remoteVideo.srcObject = null;
+				remoteVideo.remove();
+			});
+		});
 
-      messages.textContent += `${peer.id}: ${localText.value}\n`;
-      localText.value = '';
-    }
-  });
+		sendBtn.addEventListener('click', onClickSend);
+		leaveBtn.addEventListener('click', () => room.close(), { once: true });
 
-  peer.on('error', console.error);
+		// [送信]ボタン
+		function onClickSend() {
+			// Send message to all of the peers in the room via websocket
+			room.send(messageText.value);
+
+			messages.textContent += `${peer.id}: ${messageText.value}\n`;
+			messageText.value = '';
+		}
+	});
+
+	peer.on('error', console.error);
 })();
